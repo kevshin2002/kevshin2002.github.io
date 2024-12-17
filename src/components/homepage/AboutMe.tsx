554b14935e907a7b347c246e0b06498575bda621
Kevin Shin <kevshin2002@gmail.com>
2024-12-17 09:56:00 -0800

    Some minor changes to aesthetics

--- src/components/homepage/AboutMe.tsx
+++ src/components/homepage/AboutMe.tsx
@@ -13,11 +13,10 @@ export const AboutMe: FunctionComponent<AboutMeProps> = ({
   descriptionComponent,
 }) => {
   return (
-    <div className="margin-top--lg">
-      <h2>About me</h2>
+    <div>
       <div className="row">
-        <div className="col col--6">{descriptionComponent}</div>
-        <div className={clsx("col col--5", styles.avatarContainer)}>
+        <div className="col col--8">{descriptionComponent}</div>
+        <div className={clsx("col col--3", styles.avatarContainer)}>
           <div className={styles.avatar}>
             <Image img={avatar} />
           </div>
--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,16 +1,21 @@
 import clsx from "clsx";
 import React, { FunctionComponent } from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 import styles from "./Hero.module.scss";
 
 export const Hero: FunctionComponent = () => {
+  const backgroundImage = useBaseUrl("/img/escape_velocity.jpg");
+
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header
+      className={clsx("hero hero--primary", styles.heroBanner)}
+      style={{ backgroundImage: `url(${backgroundImage})` }} // Add inline background image
+    >
       <div className="container">
         <h1 className={clsx("hero__title", styles.title)}>
           Hey, I'm <span className={styles.highlighted}>Kevin</span>,
           <br />
-          <span className={styles.highlighted}>an Electrical Engineering Student</span>.
         </h1>
         <p className={clsx("hero__subtitle", styles.subtitle)}>
           Software engineer interested in continuous personal growth.
